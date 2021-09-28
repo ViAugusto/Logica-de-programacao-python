a0d22445f6493bb8903198ddb7e550d850da48df
Vinícius Augusto <81242635+ViAugusto@users.noreply.github.com>
2021-09-27 22:57:09 -0300

    Update RM92244_EX01.py

--- RM92244_EX01.py
+++ RM92244_EX01.py
@@ -3,19 +3,21 @@ altura = float(input("Digite sua altura: "))
 
 imc = peso / (altura * altura)
 
+print("Seu imc é {}!".format(imc))
+
 if(imc < 16):
-    print("Seu imc é {}!".format(imc) + " e você está na categoria Baixo peso Grau III")
+    print(" e você está na categoria Baixo peso Grau III")
 elif((imc >=16) and (imc <= 16.99)):
-    print("Seu imc é {}!".format(imc) + " e você está na categoria Baixo peso Grau II")
+    print(" e você está na categoria Baixo peso Grau II")
 elif((imc >=17) and (imc <= 18.49)):
-    print("Seu imc é {}!".format(imc) + " e você está na categoria Baixo peso Grau I")
+    print(" e você está na categoria Baixo peso Grau I")
 elif((imc >=18.50) and (imc <= 24.99)):
-    print("Seu imc é {}!".format(imc) + " e você está na categoria Peso ideal")
+    print(" e você está na categoria Peso ideal")
 elif((imc >=25) and (imc <= 29.99)):
-    print("Seu imc é {}!".format(imc) + " e você está na categoria Sobrepeso")
+    print(" e você está na categoria Sobrepeso")
 elif((imc >=30) and (imc <= 34.99)):
-    print("Seu imc é {}!".format(imc) + " e você está na categoria Obesidade Grau II")
+    print(" e você está na categoria Obesidade Grau II")
 elif((imc >=35) and (imc <= 39.99)):
-    print("Seu imc é {}!".format(imc) + " e você está na categoria Obesidade Grau II")
+    print(" e você está na categoria Obesidade Grau II")
 elif(imc >=40):
-    print("Seu imc é {}!".format(imc) + " e você está na categoria Obesidade Grau II")
+    print(" e você está na categoria Obesidade Grau II")
