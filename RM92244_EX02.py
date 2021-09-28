1ecda2e8e5ff2e46799919f2f8140117e13ba4f0
Vinícius Augusto <81242635+ViAugusto@users.noreply.github.com>
2021-09-27 22:05:05 -0300

    Create RM92244_EX02.py

---
+++ RM92244_EX02.py
@@ -0,0 +1,29 @@
+print("Seja bem vindo a Produção Multimídia da FIAP ON!!")
+print("---------------------------")
+print("Temos 4 tipos de assinturas para você, querido cliente, e elas são:")
+print("---------------------------")
+print("Nível Basic, com 30% de faturamento. Para selecionar ela digite 'B' ")
+print("Nível Silver, com 20% de faturamento. Para selecionar ela digite 'S' ")
+print("Nível Gold, com 10% de faturamento. Para selecionar ela digite 'G' ")
+print("Nível Platinum, com 5% de faturamento. Para selecionar ela digite 'P' ")
+print("---------------------------")
+
+
+assinatura = input("Digite o tipo de assinatura: ")
+faturamento = input("Digite quanto você faturou em um ano: ")
+faturamento = float(faturamento)
+
+if (assinatura == 'B'):
+    bonus = faturamento * 0.3
+    print("Você terá de pagar R${}!".format(bonus) + " de bônus como cliente Basic.")
+elif (assinatura == 'S'):
+    bonus = faturamento * 0.2
+    print("Você terá de pagar R${}!".format(bonus) + " de bônus como cliente Silver.")
+elif (assinatura == 'G'):
+    bonus = faturamento * 0.1
+    print("Você terá de pagar R${}!".format(bonus) + " de bônus como cliente Gold.")
+elif (assinatura == 'P'):
+    bonus = faturamento * 0.05
+    print("Você terá de pagar R${}!".format(bonus) + " de bônus como cliente Platinum.")
+else:
+    print("Ops, opção inválida.")
